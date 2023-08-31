8ce1a709af252d62c8ed6025c194dc98c354ddad
christokarsky <christokarsky1@gmail.com>
2023-08-30 19:08:44 -0500

    fixed route

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,4 +1,5 @@
 /**
+/**
  * Defines the router for reservation resources.
  *
  * @type {Router}
@@ -6,7 +7,30 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+router
+  .route("/:reservation_id/edit")
+  .get(controller.read)
+  .put(controller.updateReserv)
+  .all(methodNotAllowed);
+
+router
+  .route("/:reservation_id/status")
+  .get(controller.read)
+  .put(controller.updateStatus)
+  .all(methodNotAllowed);
+
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.updateReserv)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
